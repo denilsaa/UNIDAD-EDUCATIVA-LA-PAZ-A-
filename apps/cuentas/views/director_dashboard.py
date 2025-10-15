e3fdf16c64f70325aef9d7c1c69759aa5e6da7e1
denilsaa <denilsonsaavedra2005@gmail.com>
2025-10-14 23:55:24 -0400

    conteo de usuarios del director_dashboard.py

--- apps/cuentas/views/director_dashboard.py
+++ apps/cuentas/views/director_dashboard.py
@@ -1,7 +1,22 @@
-# apps/cuentas/views/director_dashboard.py
+# En cuentas/views/director_dashboard.py
 
 from django.shortcuts import render
+from apps.cuentas.models import Usuario
 
 def director_dashboard(request):
-    # Renderizar la plantilla correspondiente para el dashboard del director
-    return render(request, 'dashboard/director_dashboard.html')
+    # Obtener todos los usuarios
+    usuarios = Usuario.objects.all()
+    
+    # Obtener conteo de usuarios por rol
+    director_count = Usuario.objects.filter(rol__nombre='Director').count()
+    regente_count = Usuario.objects.filter(rol__nombre='Regente').count()
+    secretaria_count = Usuario.objects.filter(rol__nombre='Secretaria').count()
+    padre_count = Usuario.objects.filter(rol__nombre='Padre de Familia').count()
+
+    return render(request, 'dashboard/director_dashboard.html', {
+        'usuarios': usuarios,
+        'director_count': director_count,
+        'regente_count': regente_count,
+        'secretaria_count': secretaria_count,
+        'padre_count': padre_count
+    })
